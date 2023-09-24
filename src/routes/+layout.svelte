65684e22642b96d256b61ae5de1743213f2f8c1e
Aditya Ghidora <aditya@ghidora.tech>
2023-09-24 18:46:20 +0530

    added qr code scanning to test it

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,26 @@
 <script>
 	import '@picocss/pico';
 	import '../app.css';
+	import { authUser } from '$lib/authStore';
 </script>
 
 <nav>
 	<ul>
 		<li>
-			<strong> Vehicle QR codes</strong>
+			<strong><a href="/"> Vehicle QR codes</a></strong>
 		</li>
 	</ul>
 	<ul class="nav-links">
-		<li>Generate</li>
-		<li>Scan</li>
-		<li><a href="/login">Sign in</a></li>
+		<li><a href="/scan">Scan</a></li>
+		{#if $authUser !== undefined}
+			<li>
+				<a href="/me">
+					<img src={$authUser.photoURL} alt={`photo of ${$authUser.displayName}`} />
+				</a>
+			</li>
+		{:else}
+			<li><a href="/login">Sign in</a></li>
+		{/if}
 	</ul>
 </nav>
 
@@ -21,5 +29,9 @@
 <style>
 	nav {
 		margin: 1rem;
+	}
+	img {
+		width: 30px;
+		border-radius: 30px;
 	}
 </style>
---
+++ src/routes/scan/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Html5QrcodeScanner } from 'html5-qrcode';
+	import { onMount } from 'svelte';
+	function onScanSuccess(decodedText, decodedResult) {
+		console.log(`Code matched = ${decodedText}`, decodedResult);
+	}
+
+	function onScanFailure(error) {
+		console.warn(`Code scan error = ${error}`);
+	}
+	onMount(() => {
+		let html5QrcodeScanner = new Html5QrcodeScanner(
+			'reader',
+			{ fps: 10, qrbox: { width: 250, height: 250 } },
+			false
+		);
+		html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+	});
+</script>
+
+<div class="container">
+	<div id="reader" />
+</div>
